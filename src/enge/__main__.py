9861f3e0cda67a47b25407133393fe66308aab0d
Daniel Diblik <ddiblik@redhat.com>
2024-11-26 15:00:31 +0100

    fix bad imports and entry point
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>

--- src/enge/__main__.py
+++ src/enge/__main__.py
@@ -19,12 +19,12 @@ def main():
     )
 
     if parsed_opts.cli_args.action == "test":
-        from src.enge.dispatch.__main__ import main as dispatch
+        from enge.dispatch.__main__ import main as dispatch
 
         sys.exit(dispatch())
 
     elif parsed_opts.cli_args.action == "report":
-        from .report.__main__ import main as report
+        from enge.report.__main__ import main as report
 
         sys.exit(report())
 
