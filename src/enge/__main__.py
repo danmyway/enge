c78cee7174c88371fc814eb78e3cfa696280ea40
Daniel Diblik <8378124+danmyway@users.noreply.github.com>
2024-11-29 10:09:06 +0100

    Fix logging and custom config path (#4)
    * fixes #2, #3
    * change log levels
    
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>

--- src/enge/__main__.py
+++ src/enge/__main__.py
@@ -1,23 +1,11 @@
 #!/usr/bin/env python3
 
-import logging
 import sys
 
 from enge.utils.opt_manager import parsed_opts
 
 
 def main():
-    loglevel = logging.INFO
-    logformat = "%(levelname)-8s | %(message)s"
-
-    if parsed_opts.cli_args.debug:
-        loglevel = logging.DEBUG
-
-    logging.basicConfig(
-        level=loglevel,
-        format=logformat,
-    )
-
     if parsed_opts.cli_args.action == "test":
         from enge.dispatch.__main__ import main as dispatch
 
